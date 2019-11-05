2f66c7d4dd0c693a91a9b7217de98f104510651a
kmou424 <dx2689282850@gmail.com>
2019-11-05 05:39:22 +0000

    build: Fix errors if user never created some directory

--- build.sh
+++ build.sh
@@ -7,6 +7,16 @@ mka bacon -j16 > build.log 2>&1
 TIME=`date --date='0 days ago' "+%H:%M:%S"`
 DATE=`date --date='0 days ago' "+%Y-%m-%d"`
 
+# Create work root directory
+if [ ! -d "~/work" ] then
+	mkdir ~/work
+fi
+
+# Create log root directory
+if [ ! -d "~/work/log" ] then
+	mkdir ~/work/log
+fi
+
 if [ ! -d "~/work/log/$DATE" ] then
         mkdir ~/work/log/$DATE
         cp build.log ~/work/log/$DATE/$TIME/build.log
