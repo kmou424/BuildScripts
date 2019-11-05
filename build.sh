290e4042996b73855f92cb4f9d05f4b420941092
kmou424 <dx2689282850@gmail.com>
2019-11-05 06:47:32 +0000

    build: Auto generate full and trimed logs

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 . build/envsetup.sh
 lunch ion_violet-userdebug
-mka bacon -j16 > build.log 2>&1
+mka bacon -j16 > build_full.log 2>&1
+
+grep -iE 'crash|error|fail|fatal' build_full.log &> build.log
 
 TIME=`date --date='0 days ago' "+%H:%M:%S"`
 DATE=`date --date='0 days ago' "+%Y-%m-%d"`
@@ -20,8 +22,10 @@ fi
 if [ ! -d "~/work/log/$DATE" ] then
         mkdir ~/work/log/$DATE
         cp build.log ~/work/log/$DATE/$TIME/build.log
+	cp build_full.log ~/work/log/$DATE/$TIME/build_full.log
 else
         cp build.log ~/work/log/$DATE/$TIME/build.log
+	cp build_full.log ~/work/log/$DATE/$TIME/build_full.log
 fi
 
 echo 'Your logs have been stored in ~/work/log/$DATE/$TIME directory"
