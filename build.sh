0102d19fc18a15a858a623429dacfae8ed8b440f
kmou424 <dx2689282850@gmail.com>
2019-11-05 11:37:40 +0000

    build: Final fix for syntax error

--- build.sh
+++ build.sh
@@ -2,36 +2,4 @@
 
 . build/envsetup.sh
 lunch havoc_violet-userdebug
-mka bacon -j16 > build_full.log 2>&1
-
-wait $!
-
-grep -iE 'crash|error|fail|fatal' build_full.log &> build.log
-
-DATE=`date --date='0 days ago' "+%Y-%m-%d"`
-TIME=`date --date='0 days ago' "+%H-%M-%S"`
-
-# Create work root directory
-if [ ! -d "/home/kmou424/work/" ]; then
-	mkdir ~/work
-fi
-
-# Create log root directory
-if [ ! -d "/home/kmou424/work/log/" ]; then
-	mkdir ~/work/log
-fi
-
-# Create log base directory
-if [ ! -d "/home/kmou424/work/log/$DATE/" ]; then
-        mkdir ~/work/log/$DATE
-fi
-
-# Create time base directory
-if [ ! -d "/home/kmou424/work/log/$DATE/$TIME/" ]; then
-	mkdir ~/work/log/$DATE/$TIME
-fi
-
-        cp build.log ~/work/log/$DATE/$TIME/build.log
-	cp build_full.log ~/work/log/$DATE/$TIME/build_full.log
-
-echo 'Your logs have been stored in ~/work/log/'$DATE'/'$TIME' directory'
+mka bacon -j16 > build_full.log 2>&1 && bash logcater.sh
