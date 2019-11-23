aefe789c0826f4eccc18a11724b70cb162e78e90
kmou424 <dx2689282850@gmail.com>
2019-11-23 17:14:15 +0800

    android: build: Remove auto gen log

--- android/build.sh
+++ android/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 . build/envsetup.sh
-lunch havoc_violet-userdebug
-mka bacon -j16 > build_full.log 2>&1 && bash logcater.sh
+lunch aosip_violet-userdebug
+mka bacon -j16 > build_full.log 2>&1
