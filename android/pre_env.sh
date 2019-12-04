9222b3c4942f6004ba5ba5d1da5f936cd70ed260
kmou424 <dx2689282850@gmail.com>
2019-12-04 11:53:09 +0800

    android: Add pre_env for prepare build environment

---
+++ android/pre_env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+KERNEL_DIRECTORY="kernel/xiaomi/sm6150"
+DEVICE_DIRECTORY="device/xiaomi/violet"
+VENDOR_DRIECTORY="vendor/xiaomi/violet"
+
+git clone https://github.com/kmou424/sena_kernel_violet-sm6150 $(KERNEL_DIRECTORY) && git clone https://github.com/kmou424/device_xiaomi_violet $(DEVICE_DRIECTORY) && git clone https://github.com/kmou424/vendor_xiaomi_violet $(VENDOR_DRIECTORY)
