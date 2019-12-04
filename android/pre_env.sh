1ad78ea6eaa2de7516f83d649b18dc14dd9edee0
kmou424 <dx2689282850@gmail.com>
2019-12-04 14:47:23 +0800

    android: pre_env: Switch vendor repo to PixelExperience-Devices

--- android/pre_env.sh
+++ android/pre_env.sh
@@ -4,7 +4,7 @@ KERNEL_DIRECTORY="kernel/xiaomi/sm6150"
 DEVICE_DIRECTORY="device/xiaomi/violet"
 VENDOR_DRIECTORY="vendor/xiaomi/violet"
 
-git clone https://github.com/kmou424/sena_kernel_violet-sm6150 $KERNEL_DIRECTORY && git clone https://github.com/kmou424/device_xiaomi_violet $DEVICE_DRIECTORY && git clone https://github.com/kmou424/vendor_xiaomi_violet $VENDOR_DRIECTORY
+git clone https://github.com/kmou424/sena_kernel_violet-sm6150 $KERNEL_DIRECTORY && git clone https://github.com/kmou424/device_xiaomi_violet $DEVICE_DRIECTORY && git clone https://github.com/PixelExperience-Devices/vendor_xiaomi_violet $VENDOR_DRIECTORY
 
 # Patcher
 bash patcher_hals.sh
