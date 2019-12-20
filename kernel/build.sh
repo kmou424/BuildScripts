7c776c88323ac03b7baefc97a78cf677c5c8028c
kmou424 <dx2689282850@gmail.com>
2019-12-20 18:18:53 +0800

    kernel: build: Add full logcater

--- kernel/build.sh
+++ kernel/build.sh
@@ -7,4 +7,4 @@ export ARCH=arm64
 export SUBARCH=arm64
 export CROSS_COMPILE_ARM32=$COMPILER_PATH/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi- 
 make O=out ARCH=arm64 vendor/violet-perf_defconfig
-make -j32 O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- && cp out/arch/arm64/boot/*dtb $HOME/work/boot/. && cd $HOME/work && ./g upload boot/*
+make -j32 O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- > build_full.log 2>&1 && cp out/arch/arm64/boot/*dtb $HOME/work/boot/. && cd $HOME/work && ./g upload boot/*
