136b611fa5ae5a6384f4472a227b4d51e1a0b6d3
kmou424 <dx2689282850@gmail.com>
2019-12-20 18:24:55 +0800

    kernel: Add logcater to gen log

---
+++ kernel/logcater.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+grep -iE 'crash|error|fail|fatal' build_full.log &> build.log
+
+DATE=`date --date='0 days ago' "+%Y-%m-%d"`
+TIME=`date --date='0 days ago' "+%H-%M-%S"`
+
+# Create work root directory
+if [ ! -d "$HOME/work/" ]; then
+	mkdir $HOME/work
+fi
+
+# Create log root directory
+if [ ! -d "$HOME/work/log/" ]; then
+	mkdir $HOME/work/log
+fi
+
+# Create log base directory
+if [ ! -d "$HOME/work/log/$DATE/" ]; then
+        mkdir $HOME/work/log/$DATE
+fi
+
+# Create time base directory
+if [ ! -d "$HOME/work/log/$DATE/$TIME/" ]; then
+	mkdir $HOME/work/log/$DATE/$TIME
+fi
+
+cp build.log $HOME/work/log/$DATE/$TIME/kernel_build.log
+cp build_full.log $HOME/work/log/$DATE/$TIME/kernel_build_full.log
+
+echo 'Your logs have been stored in ~/work/log/'$DATE'/'$TIME' directory'
+echo 'Be careful: These are kernel build log,you should check kernel_* files'
