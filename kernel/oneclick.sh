5eba8ef5a735479facae113fd2efb08e4f05c6a7
kmou424 <dx2689282850@gmail.com>
2020-02-04 14:26:37 +0800

    Fix: oneclick script fix

--- kernel/oneclick.sh
+++ kernel/oneclick.sh
@@ -6,7 +6,7 @@ KERNEL_VERSION=$2
 printf "* Clean up previous build files\n\n"
 rm -rf out
 printf "* Begin to build\n\n"
-bash build.sh && bash pack_dtbo.sh && pack_kernel $KERNEL_VERSION
+bash build.sh && bash pack_dtbo.sh && bash pack_kernel.sh $KERNEL_VERSION
 printf "* Uploading kernel\n\n"
 mv SenaKernel* ../SenaKernel/$1
 cd ../SenaKernel/$1
