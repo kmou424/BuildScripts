6a6bdd044141b2b546fcb9340f666d725f85d7b1
kmou424 <dx2689282850@gmail.com>
2020-02-06 13:53:36 +0800

    kernel: Auto output md5&sha1 in commmit message

--- kernel/oneclick.sh
+++ kernel/oneclick.sh
@@ -8,9 +8,15 @@ rm -rf out
 printf "* Begin to build\n\n"
 bash build.sh && bash pack_dtbo.sh && bash pack_kernel.sh $KERNEL_VERSION
 printf "* Uploading kernel\n\n"
+
 mv SenaKernel* ../SenaKernel/$1
 cd ../SenaKernel/$1
 git add .
-git commit -m "Update $KERNEL_VERSION"
+git commit -m "Auto Upload $KERNEL_VERSION
+
+File Name: $(ls SenaKernel-$KERNEL_VERSION*)
+MD5: $(md5sum SenaKernel-$KERNEL_VERSION*)
+SHA1: $(sha1sum SenaKernel-$KERNEL_VERSION*)
+"
 git push --force
 printf "* Upload succeed\n"
