7d2e9afe3f1a400298d5cdd64d961c8345f8e94b
kmou424 <dx2689282850@gmail.com>
2020-03-05 10:59:53 +0800

    kernel: Add caf tag merger for 4.14
    Usage:
    On your kernel directory:
    . tagmerger.sh <TagName> <MergeSomething'sName or Number>
    
    Perference:
    1 : kernel
    2 : qcald-3.0
    3 : qca-wifi-host-cmn
    4 : fw-api
    5 : audio-kernel

---
+++ kernel/tagmerger.sh
@@ -0,0 +1,11 @@
+if [ $2 == "kernel" -o $2 == "1" ]; then
+git fetch https://source.codeaurora.org/quic/la/kernel/msm-4.14 $1&&git merge FETCH_HEAD
+elif [ $2 == "qcacld" -o $2 == "2" ]; then
+git fetch https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0 $1&&git merge FETCH_HEAD
+elif [ $2 == "qca-wifi" -o $2 == "3" ]; then
+git fetch https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn $1&&git merge FETCH_HEAD
+elif [ $2 == "fw-api" -o $2 == "4" ]; then
+git fetch https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/fw-api $1&&git merge FETCH_HEAD
+elif [ $2 == "audio" -o $2 == "5" ]; then
+git fetch https://source.codeaurora.org/quic/la/platform/vendor/opensource/audio-kernel $1&&git merge FETCH_HEAD
+fi
