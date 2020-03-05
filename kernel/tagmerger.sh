f761ddff7920901d8cb0873120fa66b88b2cafa4
kmou424 <dx2689282850@gmail.com>
2020-03-05 11:15:26 +0800

    kernel: Add auto merge all tag support
    Usage:
    . tagmerger.sh <TagName> <"all" or number "0">

--- kernel/tagmerger.sh
+++ kernel/tagmerger.sh
@@ -8,4 +8,10 @@ elif [ $2 == "fw-api" -o $2 == "4" ]; then
 git fetch https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/fw-api $1&&git merge FETCH_HEAD
 elif [ $2 == "audio" -o $2 == "5" ]; then
 git fetch https://source.codeaurora.org/quic/la/platform/vendor/opensource/audio-kernel $1&&git merge FETCH_HEAD
+elif [ $2 == "all" -o $2 == "0" ]; then
+git fetch https://source.codeaurora.org/quic/la/kernel/msm-4.14 $1&&git merge FETCH_HEAD
+git fetch https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0 $1&&git merge FETCH_HEAD
+git fetch https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn $1&&git merge FETCH_HEAD
+git fetch https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/fw-api $1&&git merge FETCH_HEAD
+git fetch https://source.codeaurora.org/quic/la/platform/vendor/opensource/audio-kernel $1&&git merge FETCH_HEAD
 fi
