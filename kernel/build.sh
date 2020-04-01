b25d9decad16a69dac32be0568db432d3fe5d332
kmou424 <dx2689282850@gmail.com>
2020-04-01 22:39:21 +0800

    Revert "kernel: build: Switch to SenaKernel"
    This reverts commit 614650883783759800206fe1df24ef9fe6bc3837.

--- kernel/build.sh
+++ kernel/build.sh
@@ -6,5 +6,5 @@ PATH="$COMPILER_PATH/clang/host/linux-x86/clang-r353983b/bin:$COMPILER_PATH/gcc/
 export ARCH=arm64
 export SUBARCH=arm64
 export CROSS_COMPILE_ARM32=$COMPILER_PATH/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi- 
-make O=out ARCH=arm64 vendor/sena-perf_defconfig
+make O=out ARCH=arm64 vendor/violet-perf_defconfig
 make $1 O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android-
