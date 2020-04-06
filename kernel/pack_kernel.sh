ace773b55da1796656aa99e7dc15ebf67934e109
DiseR-LawnX <dx2689282850@gmail.com>
2020-04-06 19:29:25 +0800

    kernel: pack: Sena -> Marisa

--- kernel/pack_kernel.sh
+++ kernel/pack_kernel.sh
@@ -7,7 +7,7 @@ cp -r flasher/* kerneltemp
 cp out/arch/arm64/boot/dtbo.img kerneltemp
 cp out/arch/arm64/boot/Image.gz-dtb kerneltemp
 cd kerneltemp
-sed -i "s/SenaKernel/$KERNEL_NAME/g" anykernel.sh
+sed -i "s/Marisa/$KERNEL_NAME/g" anykernel.sh
 zip -q -r $KERNEL_NAME.zip *
 mv *.zip ..
 cd .. && rm -rf kerneltemp
