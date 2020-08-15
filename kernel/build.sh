0df95523ebd8d1daad8318b521a4aefe5bf0abde
kmou424 <dx2689282850@gmail.com>
2020-08-15 16:47:19 +0000

    kernel: update clang to r383902c

--- kernel/build.sh
+++ kernel/build.sh
@@ -2,7 +2,7 @@
 
 COMPILER_PATH=$HOME/work/lineageos/prebuilts
 
-PATH="$HOME/work/lineageos/prebuilts/clang/host/linux-x86/clang-r353983b/bin:$COMPILER_PATH/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:${PATH}"
+PATH="$HOME/work/lineageos/prebuilts/clang/host/linux-x86/clang-r383902c/bin:$COMPILER_PATH/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:${PATH}"
 export ARCH=arm64
 export SUBARCH=arm64
 export CROSS_COMPILE_ARM32=$COMPILER_PATH/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi- 
